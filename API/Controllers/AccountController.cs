1b6a4168943be1e1e5926b5b4380e6727835b2b8
jinakuma <utlumurat@gmail.com>
2021-12-24 20:07:49 +0300

    Pagination and caching  added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,7 +46,8 @@ namespace API.Controllers
             {
                 UserName= user.UserName,
                 Token =_tokenService.CreateToken(user),
-                KnownAs =user.KnownAs
+                KnownAs =user.KnownAs,
+                Gender = user.Gender
             };
         }
         private async Task<bool> UserExists(string username)
@@ -72,7 +73,8 @@ namespace API.Controllers
                 UserName= user.UserName,
                 Token =_tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -7,7 +7,9 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { TimeagoModule } from 'ngx-timeago';
 @NgModule({
   declarations: [],
   imports: [
@@ -21,6 +23,9 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     NgxSpinnerModule,
     FileUploadModule,
     BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
+    TimeagoModule.forRoot(),
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
@@ -31,6 +36,9 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     NgxSpinnerModule,
     FileUploadModule,
     BsDatepickerModule,
+    PaginationModule,
+    ButtonsModule,
+    TimeagoModule,
   ],
 })
 export class SharedModule {}
